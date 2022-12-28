307dd86dca1c16164e914908e0c847b6976a8681
smilingbig <richard.cargill@pm.me>
2022-12-28 16:26:01 +0000

    fixing order of uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@ source ./config.sh
 
 _remove_dotfiles "$HOME/dotfiles"
 _remove_packages "${__PACKAGES[@]}"
-_remove_directories "${__DIRS[@]}"
 _remove_cargo_packages "${__CARGO_PACKAGES[@]}"
 _remove_rust "$HOME/Repos"
+_remove_directories "${__DIRS[@]}"
 _clean_packages
